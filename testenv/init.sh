878927cab8561a5759b8cbd19dce3c2f3a19405c
spawmc <spawnmcsqrt@gmail.com>
2022-01-04 13:14:30 -0600

    Example 1

---
+++ testenv/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+  
+# Script de inicialización de ambiente de pruebas
+# Aqui se pueden agregar instrucciones para inicializar el ambiente de pruebas
+# Ejemplo:
+#   dir="/home/user/testenv"
+#   mkdir -p $dir
+#   touch $dir/test.txt
+#   echo "hola mundo" > $dir/test.txt
+#   pacman -S git
+
+pacman -S nmap --noconfirm
